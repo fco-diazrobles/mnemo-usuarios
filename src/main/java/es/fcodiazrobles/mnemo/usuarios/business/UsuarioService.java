36b769fd5c95201db7d17a29e56d2de095dbe5f4
Fran Díaz <franciscojosediaz@ibermutua.es>
2021-02-25 14:30:18 +0100

    Services

---
+++ src/main/java/es/fcodiazrobles/mnemo/usuarios/business/UsuarioService.java
@@ -0,0 +1,13 @@
+package es.fcodiazrobles.mnemo.usuarios.business;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Service para operaciones en BD de la Tabla Usuario
+ * 
+ * @author Francisco José Díaz Robles
+ *
+ */
+@Service
+public interface UsuarioService {
+}
---
+++ src/main/java/es/fcodiazrobles/mnemo/usuarios/business/impl/UsuarioServiceImpl.java
@@ -0,0 +1,18 @@
+package es.fcodiazrobles.mnemo.usuarios.business.impl;
+
+import org.springframework.stereotype.Service;
+
+import es.fcodiazrobles.mnemo.usuarios.business.UsuarioService;
+
+/**
+ * Implementanción de UsuarioService
+ * 
+ * @author Francisco José Díaz Robles
+ *
+ */
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+   
+}
